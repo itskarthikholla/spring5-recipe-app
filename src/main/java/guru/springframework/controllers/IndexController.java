a1bdf4a9698fd984cb528a99e435f3501ed148be
Karthik Holla <kholla@cisco.com>
2020-08-01 14:47:49 +0530

    Before Lombok Assingnment

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,7 +1,17 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.Recipe;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.domain.repositories.CategoryRepository;
+import guru.springframework.domain.repositories.UnitOfMeasureRepository;
+import guru.springframework.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
@@ -9,8 +19,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService){
+        this.recipeService = recipeService;
+
+    }
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model) {
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
